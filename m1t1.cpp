5f52e3b139bbdca8bf884ecaf7068064bfc93d69
Fx-Arkan <chain4jc@hotmail.com>
2025-01-30 01:25:51 +0000

    updating info

---
+++ m1t1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//M1T1
+//Hello World
+// Efrain Alvarado
+//Jan. 13 2025
+
+int main() {
+
+  cout << "Hello World!\n";
+  string name = " Efrain Alvarado";
+  cout << "Nice to meet you, "<< name << endl;
+  
+}
