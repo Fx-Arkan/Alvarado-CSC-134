cd7edc428e665cab3065532385ba43e14a0e7b1c
Fx-Arkan <chain4jc@hotmail.com>
2025-02-09 22:53:43 +0000

    update3

---
+++ Alvarado_M2T2.cpp
@@ -0,0 +1,35 @@
+// CSC 134
+// M2T2
+// Efrain Alvarado
+2/9/2025
+
+#include <iostream>
+
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+  
+  double each = 0.50 + 0.02;
+  double costumer; 
+  cout << "Price of apples today is 0.50 cents each." << endl;
+  cout << "How many apples are you purchasing today?  ";
+  cin >> costumer;
+  double total = costumer * each;
+
+  cout << fixed <<setprecision(2);
+  cout << "Your total is: $" << total << endl;
+
+  int age;
+  cout << "and what is your age: ";
+  cin >> age; 
+  cout << "Sorry, but also your age is added to the price." << endl;
+  double balance = age * total;
+  cout << "your adjusted balance now is: $" <<  balance << endl;
+  cout << "Thank You for shopping with us, please come again." << endl;
+  
+  return 0;
+
+}
