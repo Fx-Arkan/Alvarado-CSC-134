b235bad5c0325da16441bb3ee8081975d31ea61e
Fx-Arkan <chain4jc@hotmail.com>
2025-02-17 12:35:14 -0500

    Update M3T1.cpp

--- M3T1.cpp
+++ M3T1.cpp
@@ -10,7 +10,15 @@
 using namespace std;
 int main()
 {
+double user, width1, width2, length1, length2, area1, area2;
+area1 = length1 * width1;
+area2 = length2 * width2;
 
+cout << " Length please";
+cin >> length1;
+cout << " next width";
+cin >> width1;
+cout << area1 << endl;
 
 
 
