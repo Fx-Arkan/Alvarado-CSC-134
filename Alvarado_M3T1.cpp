2641b323e7f051a7dfaffc0640fb342b48bd4160
Fx-Arkan <chain4jc@hotmail.com>
2025-02-17 21:46:06 +0000

    up1

---
+++ Alvarado_M3T1.cpp
@@ -0,0 +1,26 @@
+// M3T1
+// class
+// Alvarado 
+// date
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main()
+{
+double user, width1, width2, length1, length2, area1, area2;
+area1 = length1 * width1;
+area2 = length2 * width2;
+
+cout << " user, Length please";
+cin >> length1;
+cout << " next width";
+cin >> width1;
+cout << area1 << endl;
+
+return 0;
+
+
+}
