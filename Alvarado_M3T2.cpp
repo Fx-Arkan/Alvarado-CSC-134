93f531c75f9bc188b9829f3c651bdb52d4656275
Fx-Arkan <chain4jc@hotmail.com>
2025-02-18 01:06:04 +0000

    up7

--- Alvarado_M3T2.cpp
+++ Alvarado_M3T2.cpp
@@ -1,40 +1,55 @@
 // M3T2
-// class
-// Alvarado 
-// date
+// CSC-134
+// Efrain Alvarado 
+// 2/17/2025
+
+/* On this one, I'm building on top of the previous assignment and continuing to work 
+on the program by adding extra questions for the user.*/
 
 #include <iostream>
 #include <iomanip>
 #include <string>
+
 using namespace std;
+
 int main()
+
 {
 
 double width1, width2, length1, length2;
-// I'm keeping it simple and just going straight to the task.
-// Also trying to implement/practice my comments.
-cout << "Hi, Rectangle-1 length please: ";
+
+cout << "Hi, the r stands for Rectangle. r1 length please: ";
 cin >> length1;
 cout << "next, the width please: ";
 cin >> width1;
 cout << "your length is: " << length1 << endl;
 cout << "and your width is: " << width1 << endl;
 double area1 = length1 * width1;
-cout << "so, the rectangle-1 area is: " << area1 << endl;
+cout << "so, r1 area is: " << area1 << endl;
 
-// Next is the measurement of the second rectangle.
 
-cout << "Now, Rectangle-2 length please: ";
+cout << "Now, r2 (Rectangle 2) length please: ";
 cin >> length2;
 cout << "next the width please: ";
 cin >> width2;
 cout << "you length is: " << length2 << endl;
 cout << "and your width is: " << width2 << endl;
 double area2 = length2 * width2;
-cout << "Rectangle-2 area is: " << area2 << endl;
+cout << "r2 area is: " << area2 << endl;
+cout << "r1 area is: " << area1 << " and r2 area is: " << area2 << endl;
+string larger, r1, r2, Correct, Worng;
+cout << "which one is larger, r1 or r2? " << endl;
+cin >> Correct;
+if (area1 > area2)
+{
+    cout << "r1 is the correct answer" << endl;
+}
+else if (area1 < area2)
+{
+    cout << "r2 is the correct answer" << endl;
+}
 
-cout << "Rectangle-1 area is: " << area1 << " and Rectangle-2 area is: " << area2 << endl;
-
+cout << "well done!" << endl;
 cout << "Thank you and have a Great Day!" << endl;
 
 return 0;
