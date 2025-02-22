47367a71fded5752f6baf458d0db490f9c398cf2
Fx-Arkan <chain4jc@hotmail.com>
2025-02-22 12:49:36 -0500

    Update Alvarado_M2LAB1.cpp

--- Alvarado_M2LAB1.cpp
+++ Alvarado_M2LAB1.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
 
-const double COST_PER_CUBIC_FOOT = 0.23;
+const double COST_PER_CUBIC_FOOT = 0.30;
 const double CHARGE_PER_CUBIC_FOOT = 0.5;
 
 double length, width, height, volume, cost, charge, profit;
@@ -31,7 +31,7 @@ charge = volume * CHARGE_PER_CUBIC_FOOT;
 profit = charge - cost;
 
 cout << "The volume of the crate is ";
-cout << volume << "cubic feet. \n";
+cout << volume << " cubic feet. \n";
 cout << "Cost to build: $" << cost << endl;
 cout << "Charge to customer: $" << charge << endl;
 cout << "Profit: $" << profit << endl;
