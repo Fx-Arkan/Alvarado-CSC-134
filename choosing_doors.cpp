a7bfd3a2b7d52c76c7bf8ba97ca67db82e3ce6f0
Fx-Arkan <chain4jc@hotmail.com>
2025-03-01 17:16:28 -0500

    u8

--- choosing_doors.cpp
+++ choosing_doors.cpp
@@ -4,7 +4,9 @@ using namespace std;
 void chooseDoor1();
 void chooseDoor2();
 
-int main() {
+int main() 
+
+{
   
   int choice; 
 
@@ -13,29 +15,33 @@ int main() {
   cout << "Type 1 or 2: "; 
   cin >> choice;
 
-  if (1 == choice) {
-    chooseDoor1();
-  }
-  else if (2 == choice) {
-    chooseDoor2();
-  }
-  else {
-    cout << "I'm sorry, that is not a valid choice." << endl;
-  }
+
+  if (1 == choice) { chooseDoor1(); }
+
+
+  else if (2 == choice) { chooseDoor2(); }
+
+  else { cout << "I'm sorry, that is not a valid choice." << endl; }
+
 
   cout << "Thank you for playing!" << endl;
+  cout << "Press Enter to continue...";
+  cin.get();
+
   return 0; 
+
 
 } 
 
- void chooseDoor1() {
- 
+ void chooseDoor1() 
+ {
   cout << "You chose Door 1" << endl;
   cout << "You win ... A NEW CAR!" << endl;
-}
+ }
 
-void chooseDoor2() {
- 
+
+void chooseDoor2() 
+ {
   cout << "You chose Door 2" << endl;
   cout << "You win ... a bottle of floor wax." << endl;
-}
+ }
