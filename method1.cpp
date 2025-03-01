b46ca0df594dcc58ae8def774ceb95c5a3e9b47d
Fx-Arkan <chain4jc@hotmail.com>
2025-03-01 09:18:52 -0500

    Update2 method1.cpp

--- method1.cpp
+++ method1.cpp
@@ -1,3 +1,33 @@
 #include <iostream>
 using namespace std.
+
+void chooseDoor1();
+void chooseDoor2();
+
+
 int main()
+{
+ int choice;
+
+cout << "choose door 1 or 2? " << endl;
+cin >> choice;
+
+if (1 == choice) { 
+chooseDoor1();
+}
+else if (2 == choice) {
+chooseDoor2();
+}
+else { cout << "sorry" << endl;
+}
+
+cout << "thanks" << endl;
+return 0;
+}
+
+
+
+
+
+
+}
