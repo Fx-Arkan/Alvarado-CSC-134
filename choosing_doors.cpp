e5bdd5c181c8cbc09c36a6ee4d2eb7c1df57eef5
Fx-Arkan <chain4jc@hotmail.com>
2025-03-01 21:31:30 -0500

    u2

--- choosing_doors.cpp
+++ choosing_doors.cpp
@@ -31,7 +31,7 @@ int main()
   system("pause"); /* system function used for pause here, 
   I noticed in the small amount I read about 'system' that it is a very important function 
   and if not handled properly it creates a security vulnerability. */ 
-  /* The reason why I'm using the function "system" is because at the end of a program,
+  /* The reason why I'm using the function 'system' is because at the end of a program,
   it goes so fast you don't get a chance to read what it says at the end before it closes the program*/
    
 
@@ -51,7 +51,7 @@ void chooseDoor2()
  {
   int hour;
   string suffix;
-  string time = "evening my friend.";
+  string time = "evening my friend. You get nothing, try again";
   cout << "Type a number between 1 and 12" << endl;
   cin >> hour;
   cout << "is it am or pm? " << endl;
@@ -60,12 +60,12 @@ void chooseDoor2()
   { 
   if (suffix == "pm")
      { if ( hour < 6 )
-        { time = "afternoon my friend."; }
+        { time = "afternoon my friend. You get a Free trip, one way to Antartica!"; }
      }
      
      else 
 
-    { time = "morning my friend."; }
+    { time = "morning my friend. You win, a Trip to Wonka's Chocolate Factory!"; }
     }
 
   
@@ -74,32 +74,35 @@ void chooseDoor2()
 
  }
 
- void chooseDoor3() 
- {
-  static int repeatCounter = 0;  // Static variable to keep track of repeats
+ void chooseDoor3() {
+    static int repeatCounter = 0;  // Static variable to keep track of repeats
 
-  int choice;
-  cout << "Try again, Door 1, Door 2, or Door 3?" << endl;
-  cout << "Type 1, 2, or 3: "; 
-  cin >> choice;
+    int choice;
+    while (true) {
+        cout << "Try again, Door 1, Door 2, or Door 3?" << endl;
+        cout << "Type 1, 2, or 3: "; 
+        cin >> choice;
 
-  if (1 == choice) {
-      chooseDoor1();
-      repeatCounter = 0;  // Reset counter
-  } else if (2 == choice) {
-      chooseDoor2();
-      repeatCounter = 0;  // Reset counter
-  } else if (3 == choice) {
-      repeatCounter++;
-      if (repeatCounter > 3) {
-          cout << "Can you make another selection? I can do this all day!" << endl;
-      } else {
-          chooseDoor3();
-      }
-  } else {
-      cout << "I'm sorry, that is not a valid choice." << endl;
-      repeatCounter = 0;  // Reset counter
-  }
- }
+        if (1 == choice) {
+            chooseDoor1();
+            repeatCounter = 0;  // Reset counter
+            break;
+        } else if (2 == choice) {
+            chooseDoor2();
+            repeatCounter = 0;  // Reset counter
+            break;
+        } else if (3 == choice) {
+            repeatCounter++;
+            if (repeatCounter > 3) {
+                cout << "Really?" << endl;
+            } else {
+                cout << "Can you make another selection? I can do this all day!" << endl;
+            }
+        } else {
+            cout << "I'm sorry, that is not a valid choice." << endl;
+            repeatCounter = 0;  // Reset counter
+        }
+    }
+}
 
- 
+  
