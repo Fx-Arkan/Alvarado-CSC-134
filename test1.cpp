aa41126c57cbf0eb8b5fc673c426296ef1edafbf
Fx-Arkan <chain4jc@hotmail.com>
2025-04-04 22:30:51 +0000

    up3

--- test1.cpp
+++ test1.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main()
 { 
   
-    int firstNum, answer;
+    int firstNum, number, answer;
     firstNum = 5;
     //secondNum = 1;
     //answer = firstNum * secondNum;
-
+    cout << "5 times your number: " << endl;
     for (int i=1; i<= 5; i++)
-    {
+    {  
         answer = firstNum * i;
-    cout << firstNum << " times " << i << " is " << answer << endl;
+    cout << firstNum << " times your number " << i << " is " << answer << endl;
     }
     
     
