2e3d478dbf62433fccd564c48f9038a2cf74cd71
Fx-Arkan <chain4jc@hotmail.com>
2025-04-05 23:26:33 +0000

    up4

--- test1.cpp
+++ test1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 { 
     // declare variables
-    int firstNum, secondNum, answer;
+    int firstNum, secondNum, answer, total;
     firstNum = 5;
 
     //secondNum = 1;
@@ -16,6 +16,12 @@ int main()
     cout << firstNum << " times " << i << " is " << answer << endl;
     
    }
+
+   cout << "Enter a number to multiply by 5: ";
+   cin >> secondNum;
+
+   total = firstNum * secondNum;
+   cout << "The total is: " << total << endl;
    
     return 0;
 }
