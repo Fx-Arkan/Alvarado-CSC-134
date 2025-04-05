a1b8a6ab3d0e0716308a765f2a7e2985191ff81f
Fx-Arkan <chain4jc@hotmail.com>
2025-04-05 23:04:04 +0000

    up3

--- test1.cpp
+++ test1.cpp
@@ -3,20 +3,19 @@
 using namespace std;
 int main()
 { 
-  
-    int firstNum, number, answer;
+    // declare variables
+    int firstNum, secondNum, answer;
     firstNum = 5;
-    
+
     //secondNum = 1;
     //answer = firstNum * secondNum;
-    cout << "5 times your number: ";
-    cin >> number;
+  
+  for (int i=1; i<= 10; i++)
+   {    
+    answer = firstNum * i;
+    cout << firstNum << " times " << i << " is " << answer << endl;
+    
+   }
    
-    
-        answer = firstNum * number;
-    cout << firstNum << " times " << number << " is " << answer << endl;
-    
-    
-    
     return 0;
 }
