c5be87a12972be3270f226d029e71e1d82ccadc7
Fx-Arkan <chain4jc@hotmail.com>
2025-04-05 21:07:01 +0000

    up2

--- test1.cpp
+++ test1.cpp
@@ -11,11 +11,11 @@ int main()
     //answer = firstNum * secondNum;
     cout << "5 times your number: ";
     cin >> number;
-    for (int i=1; i<= 1; i++)
-    {  
+   
+    
         answer = firstNum * number;
     cout << firstNum << " times " << number << " is " << answer << endl;
-    }
+    
     
     
     return 0;
